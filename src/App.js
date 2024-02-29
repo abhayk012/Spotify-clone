915d6fd4a3ee2148cd37800cb4d80198ea966fe4
abhayk012 <abhaykrishnan012@gmail.com>
2024-02-29 13:38:05 +0530

    Spotify clone

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Leftside from './components/Leftside';
+import Rightside from './components/Rightside';
+
+
+function App() {
+  return (
+    <div className="App border rounded-3 text-white bg-dark">
+     <Leftside/>
+     <Rightside/>
+      
+    </div>
+  );
+}
+
+export default App;
